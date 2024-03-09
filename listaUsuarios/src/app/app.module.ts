0359256a20ad5d6864f02e3c35fb3fbd09372aa4
Leonardo Vechieti <leonardovechieti@outlook.com>
2024-03-09 16:44:01 -0300

    update: estrutura do projeto

--- listaUsuarios/src/app/app.module.ts
+++ listaUsuarios/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponentsModule } from './components/components.module';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { AngularMaterialModule } from './angular-material/angular-material.module';
+
 
 @NgModule({
   declarations: [
@@ -13,6 +15,7 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
   imports: [
     BrowserModule,
     AppRoutingModule,
+    AngularMaterialModule,
     ComponentsModule
   ],
   providers: [
