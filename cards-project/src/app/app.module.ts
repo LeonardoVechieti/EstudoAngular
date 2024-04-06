6804012fe66c8750824adc93eee457fe24461048
Leonardo Vechieti <leonardovechieti@outlook.com>
2024-04-06 16:51:51 -0300

    feat: estruturando os cards

--- cards-project/src/app/app.module.ts
+++ cards-project/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BalanceCardComponent } from './components/balance-card/balance-card.component';
+import { EditCardComponent } from './components/edit-card/edit-card.component';
+import { DebitCardComponent } from './components/debit-card/debit-card.component';
+import { ClientInfosCardComponent } from './components/client-infos-card/client-infos-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BalanceCardComponent,
+    EditCardComponent,
+    DebitCardComponent,
+    ClientInfosCardComponent
   ],
   imports: [
     BrowserModule
