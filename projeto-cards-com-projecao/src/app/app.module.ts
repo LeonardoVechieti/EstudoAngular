b9574c2a38c25c963b88b1d579e9f0e0203bae72
Leonardo Vechieti <leonardovechieti@outlook.com>
2024-04-11 21:13:27 -0300

    inicio da refatoração do projeto cards

---
+++ projeto-cards-com-projecao/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { BalanceCardComponent } from './components/balance-card/balance-card.component';
+import { CreditCardComponent } from './components/credit-card/credit-card.component';
+import { DebitCardComponent } from './components/debit-card/debit-card.component';
+import { ClientInfosCardComponent } from './components/client-infos-card/client-infos-card.component';
+import { CardItemComponent } from './components/card-item/card-item.component';
+import { CardLineComponent } from './components/card-line/card-line.component';
+import { CardTitleComponent } from './components/card-title/card-title.component';
+import { CardComponent } from './components/card/card.component';
+import { CardHeaderDirective } from './components/card/directives/card-header.directive';
+import { CardContentDirective } from './components/card/directives/card-content.directive';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BalanceCardComponent,
+    CreditCardComponent,
+    DebitCardComponent,
+    ClientInfosCardComponent,
+    CardItemComponent,
+    CardLineComponent,
+    CardTitleComponent,
+    CardComponent,
+    CardHeaderDirective,
+    CardContentDirective
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
